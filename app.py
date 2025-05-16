d3ef4ae589f1c6d4e98d36eefa847936b1d26502
AjayPlus <53108165+AjayPlus@users.noreply.github.com>
2025-05-15 18:42:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
-from flask_cors import CORS
-import os
 
 app = Flask(__name__)
-CORS(app)
 
 clf = pipeline("text-classification", model="AjayPlus/propaganda-detector-v2")
 
-@app.route("/", methods=["GET"])
-def home():
-    return "✅ Flask API is running"
-
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    try:
-        text = request.json.get("inputs", "")
-        result = clf(text)
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    data = request.get_json()
+    inputs = data.get("inputs", "")
+    result = clf(inputs)
+    return jsonify(result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=7860)
